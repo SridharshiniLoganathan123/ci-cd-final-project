7dcc4801c8c5b50bd788b78252ac69380b8913f1
SridharshiniLoganathan123 <146981302+SridharshiniLoganathan123@users.noreply.github.com>
2024-05-16 23:28:12 -0700

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,7 +1,8 @@
+
 """
 Controller for routes
 """
-from flask import jsonify, url_for, abort
+from flask import jsonify, url_for
 from service import app
 from service.common import status
 
@@ -20,6 +21,8 @@ def health():
 ############################################################
 # Index page
 ############################################################
+
+
 @app.route("/")
 def index():
     """Returns information abut the service"""
@@ -40,9 +43,7 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
-
-    return jsonify(counters)
+    return jsonify({"error": "Not Found"}), 404  # ./service/routes.py:78
 
 
 ############################################################
@@ -54,10 +55,9 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return jsonify({"error": "Not Found"}), 404  # ./service/routes.py:78
 
     COUNTER[name] = 0
-
     location_url = url_for("read_counters", name=name, _external=True)
     return (
         jsonify(name=name, counter=0),
@@ -75,7 +75,7 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return jsonify({"error": "Not Found"}), 404  # ./service/routes.py:78
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -90,8 +90,7 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
-
+        return jsonify({"error": "Not Found"}), 404  # ./service/routes.py:78
     COUNTER[name] += 1
 
     counter = COUNTER[name]
